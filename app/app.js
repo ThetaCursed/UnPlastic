5ac25866e6b288ca184cbdd3f2c700919f4b362b
ThetaCursed <thetacursed@gmail.com>
2026-03-31 14:37:47 +0300

    Auto-update

--- app/app.js
+++ app/app.js
@@ -767,7 +767,7 @@ function waitForWorkerAndApplyFilters() {
                 proceedWithImage(demoImage, mockFile, true);
             };
             demoImage.onerror = () => console.error("Could not load the demo image.");
-            demoImage.src = './images/example.png';
+            demoImage.src = './app/images/example.png';
         } else {
             // Повторный визит: просто загружаем сохраненные значения ползунков
             loadSliderValues();
